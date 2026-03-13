43b7f3420502375bffe620865c9ea9fde10945a4
Joao Vitor M <joao.vitorms1509@gmail.com>
2026-03-13 03:47:55 -0300

    Initial commit

---
+++ estudante-service/src/main/java/org/zsh/estudante_service/EstudanteServiceApplication.java
@@ -0,0 +1,13 @@
+package org.zsh.estudante_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudanteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudanteServiceApplication.class, args);
+	}
+
+}
---
+++ matricula-service/src/main/java/org/zsh/matricula_service/MatriculaServiceApplication.java
@@ -0,0 +1,13 @@
+package org.zsh.matricula_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaServiceApplication.class, args);
+	}
+
+}
